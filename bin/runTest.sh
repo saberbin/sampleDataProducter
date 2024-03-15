e0a9c29e99a126ccb41d8365cee65796598de775
saberbin <saberbin0921@gmail.com>
2024-03-15 15:57:28 +0800

    示例启动脚本

--- bin/runTest.sh
+++ bin/runTest.sh
@@ -1,4 +1,4 @@
-cd /home/saberbin/project/sampleKafkaProducter
-# python sampleKafkaProducter.py --config /home/saberbin/project/sampleKafkaProducter/settings/defaultConf_text.conf
-/home/saberbin/miniconda3/envs/pyspark/bin/python sampleKafkaProducter.py --config /home/saberbin/project/sampleKafkaProducter/settings/defaultConf_text.conf
+cd /home/saberbin/project/sampleDataProducter
+# python sampleDataProducter.py --config /home/saberbin/project/sampleDataProducter/settings/defaultConf_text.conf
+/home/saberbin/miniconda3/envs/pyspark/bin/python sampleDataProducter.py --config /home/saberbin/project/sampleDataProducter/settings/defaultConf_text.conf
 
